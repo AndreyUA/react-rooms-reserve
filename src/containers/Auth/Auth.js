8b765acec97557f046e53411a3ab1a3c1214b044
AndreyUA <infernalsteel666@gmail.com>
2020-07-17 17:29:54 +0300

    7-17-2020

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,18 +1,71 @@
 import React, { Component } from "react";
 import "./Auth.css";
 
+import is from "is_js";
+
 class Auth extends Component {
   state = {
+    isEmailValid: true,
+    isPasswordValid: true,
     email: {
       value: "",
       errorMessage: "Enter correct email",
-      isFormValid: true,
     },
     password: {
       value: "",
       errorMessage: "Enter correct password",
-      isFormValid: true,
     },
+  };
+
+  loginHandler = () => {};
+
+  registerHandler = () => {};
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    console.log(
+      `login: ${this.state.email.value}, password: ${this.state.password.value}`
+    );
+  };
+
+  emailChangeHandler = (e) => {
+    const value = e.target.value;
+    const email = { ...this.state.email };
+
+    let isValid = true;
+
+    if (value.trim() === "" || !is.email(value)) {
+      isValid = false;
+    }
+
+    if (isValid) {
+      email.value = value;
+    }
+
+    this.setState({
+      isEmailValid: isValid,
+      email,
+    });
+  };
+
+  passwordChangeHandler = (e) => {
+    const value = e.target.value;
+    const password = { ...this.state.password };
+
+    let isValid = true;
+
+    if (value.length <= 5) {
+      isValid = false;
+    }
+
+    if (isValid) {
+      password.value = value;
+    }
+
+    this.setState({
+      isPasswordValid: isValid,
+      password,
+    });
   };
 
   render() {
@@ -20,13 +73,13 @@ class Auth extends Component {
       <div className="Auth">
         <div className="Auth-block">
           <h1>Login</h1>
-          <form>
+          <form onSubmit={this.submitHandler}>
             <div className="Auth-email">
               <label htmlFor="Auth-email">Email:</label>
-              <input id="Auth-email" />
+              <input onChange={this.emailChangeHandler} id="Auth-email" />
             </div>
 
-            {this.state.email.isFormValid ? (
+            {this.state.isEmailValid ? (
               <p className="Auth-correct">Enter your email</p>
             ) : (
               <p className="Auth-error">{this.state.email.errorMessage}</p>
@@ -34,16 +87,32 @@ class Auth extends Component {
 
             <div className="Auth-pass">
               <label htmlFor="Auth-pass">Password:</label>
-              <input id="Auth-pass" />
+              <input onChange={this.passwordChangeHandler} id="Auth-pass" />
             </div>
-            {this.state.password.isFormValid ? (
+            {this.state.isPasswordValid ? (
               <p className="Auth-correct">Enter your password</p>
             ) : (
               <p className="Auth-error">{this.state.password.errorMessage}</p>
             )}
             <div className="Auth-control">
-              <button className="Auth-log">Login</button>
-              <button className="Auth-reg">Registration</button>
+              <button
+                className="Auth-log"
+                disabled={
+                  !this.state.isEmailValid || !this.state.isPasswordValid
+                }
+                onClick={this.loginHandler}
+              >
+                Login
+              </button>
+              <button
+                className="Auth-reg"
+                disabled={
+                  !this.state.isEmailValid || !this.state.isPasswordValid
+                }
+                onClick={this.registerHandler}
+              >
+                Registration
+              </button>
             </div>
           </form>
         </div>
@@ -53,7 +122,3 @@ class Auth extends Component {
 }
 
 export default Auth;
-
-//нужно сделать форму авторизации с валидацией
-
-//остались кнопки и обработчики
